7b3a479e86abfc7b6f68c4388d2853162770256d
blepping <blepping@tuta.io>
2025-08-20 19:56:19 -0600

    Tile Chroma Radiance NeRF to reduce memory consumption, update memory usage factor

--- comfy/supported_models.py
+++ comfy/supported_models.py
@@ -1212,7 +1212,7 @@ class ChromaRadiance(Chroma):
     latent_format = comfy.latent_formats.ChromaRadiance
 
     # Pixel-space model, no spatial compression for model input.
-    memory_usage_factor = 0.75
+    memory_usage_factor = 0.0325
 
     def get_model(self, state_dict, prefix="", device=None):
         return model_base.ChromaRadiance(self, device=device)
