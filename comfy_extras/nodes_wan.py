8d13c34b826860dd898dc017830a47d81834ed2d
silveroxides <ishimarukaito@gmail.com>
2025-10-29 06:00:31 +0100

    wan changes, logic, math, primitive

--- comfy_extras/nodes_wan.py
+++ comfy_extras/nodes_wan.py
@@ -12,6 +12,9 @@ from typing import Tuple
 from typing_extensions import override
 from comfy_api.latest import ComfyExtension, io
 
+def round_to_nearest(n, m):
+    return int((n + (m / 2)) // m) * m
+
 class WanImageToVideo(io.ComfyNode):
     @classmethod
     def define_schema(cls):
@@ -26,6 +29,7 @@ class WanImageToVideo(io.ComfyNode):
                 io.Int.Input("height", default=480, min=16, max=nodes.MAX_RESOLUTION, step=16),
                 io.Int.Input("length", default=81, min=1, max=nodes.MAX_RESOLUTION, step=4),
                 io.Int.Input("batch_size", default=1, min=1, max=4096),
+                io.Boolean.Input("tiled_vae", default=False),
                 io.ClipVisionOutput.Input("clip_vision_output", optional=True),
                 io.Image.Input("start_image", optional=True),
             ],
@@ -37,14 +41,23 @@ class WanImageToVideo(io.ComfyNode):
         )
 
     @classmethod
-    def execute(cls, positive, negative, vae, width, height, length, batch_size, start_image=None, clip_vision_output=None) -> io.NodeOutput:
+    def execute(cls, positive, negative, vae, width, height, length, batch_size, tiled_vae, start_image=None, clip_vision_output=None) -> io.NodeOutput:
         latent = torch.zeros([batch_size, 16, ((length - 1) // 4) + 1, height // 8, width // 8], device=comfy.model_management.intermediate_device())
         if start_image is not None:
             start_image = comfy.utils.common_upscale(start_image[:length].movedim(-1, 1), width, height, "bilinear", "center").movedim(1, -1)
             image = torch.ones((length, height, width, start_image.shape[-1]), device=start_image.device, dtype=start_image.dtype) * 0.5
             image[:start_image.shape[0]] = start_image
 
-            concat_latent_image = vae.encode(image[:, :, :, :3])
+            if tiled_vae:
+                tile_x = round_to_nearest(int(width*0.5), int(32))
+                tile_y = round_to_nearest(int(height*0.5), int(32))
+                overlap = round_to_nearest(int((tile_x + tile_y)/4), int(32))
+                tile_t = round_to_nearest(int((length - 1)*0.5), int(4))
+                overlap_t = round_to_nearest(int(tile_t / 8), int(4))
+                print(f"tile_x={tile_x}, tile_y={tile_y}, overlap={overlap}, tile_t={tile_t}, overlap_t={overlap_t}")
+                concat_latent_image = vae.encode_tiled(image[:,:,:,:3], tile_x=tile_x, tile_y=tile_y, overlap=overlap, tile_t=tile_t, overlap_t=overlap_t)
+            else:
+                concat_latent_image = vae.encode(image[:, :, :, :3])
             mask = torch.ones((1, 1, latent.shape[2], concat_latent_image.shape[-2], concat_latent_image.shape[-1]), device=start_image.device, dtype=start_image.dtype)
             mask[:, :, :((start_image.shape[0] - 1) // 4) + 1] = 0.0
 
