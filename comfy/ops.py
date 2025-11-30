31b0ac4debe29335b56338526ce19f0bfff1eb02
silveroxides <ishimarukaito@gmail.com>
2025-11-30 15:58:24 +0100

    Fix cases where scale_weight is missing when loading LoRA

--- comfy/ops.py
+++ comfy/ops.py
@@ -502,6 +502,8 @@ def scaled_fp8_ops(fp8_matrix_mult=False, scale_input=False, override_dtype=None
                 return x
 
             def convert_weight(self, weight, inplace=False, **kwargs):
+                if self.scale_weight is None:
+                    return weight
                 if inplace:
                     weight *= self.scale_weight.to(device=weight.device, dtype=weight.dtype)
                     return weight
@@ -509,6 +511,8 @@ def scaled_fp8_ops(fp8_matrix_mult=False, scale_input=False, override_dtype=None
                     return weight.to(dtype=torch.float32) * self.scale_weight.to(device=weight.device, dtype=torch.float32)
 
             def set_weight(self, weight, inplace_update=False, seed=None, return_weight=False, **kwargs):
+                if self.scale_weight is None:
+                    return weight
                 weight = comfy.float.stochastic_rounding(weight / self.scale_weight.to(device=weight.device, dtype=weight.dtype), self.weight.dtype, seed=seed)
                 if return_weight:
                     return weight
