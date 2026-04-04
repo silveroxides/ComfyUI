74da7602d58e78b6255d91611c0a233b5550a6b4
silveroxides <ishimarukaito@gmail.com>
2026-04-05 01:21:20 +0200

    reset some local changes

--- nodes.py
+++ nodes.py
@@ -2433,7 +2433,6 @@ async def init_builtin_extra_nodes():
         "nodes_tcfg.py",
         "nodes_context_windows.py",
         "nodes_qwen.py",
-        "nodes_logic.py",
         "nodes_chroma_radiance.py",
         "nodes_model_patch.py",
         "nodes_easycache.py",
